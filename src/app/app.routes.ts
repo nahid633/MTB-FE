7ae9a9f86b20a059b15151c722d9c7a899c13613
Nahid Guliyev <quliyevn633@gmail.com>
2023-12-17 07:42:35 +0100

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: 'test', loadComponent: () => import('./components/test-location/test-location.component').then(component => component.TestLocationComponent)},
+  {path: '', loadComponent: () => import('./pages/landing/landing.component').then(component => component.LandingComponent)},
+  {path: 'booking', loadComponent: () => import('./pages/booking/booking.component').then(component => component.BookingComponent)},
+  {path: 'certification', loadComponent: () => import('./pages/certification/certification.component').then(component => component.CertificationComponent)},
+  {path: 'play-simulation', loadComponent: () => import('./pages/play-simulation/play-simulation.component').then(component => component.PlaySimulationComponent)}
+];
