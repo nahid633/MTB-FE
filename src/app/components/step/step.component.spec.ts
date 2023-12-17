9a13eb84eadc3662aef9de27689f48ec27a41ddc
Nahid Guliyev <quliyevn633@gmail.com>
2023-12-17 07:44:07 +0100

    step component

---
+++ src/app/components/step/step.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { StepComponent } from './step.component';
+import {MatCardModule} from "@angular/material/card";
+import {MatProgressBarModule} from "@angular/material/progress-bar";
+
+describe('StepComponent', () => {
+  let component: StepComponent;
+  let fixture: ComponentFixture<StepComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [StepComponent, MatCardModule, MatProgressBarModule ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(StepComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
